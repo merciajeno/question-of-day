d94383c7af13483c509e42d72f8ee6ab41bd9fca
issac <issac.rajan@gmail.com>
2023-12-31 11:00:33 +0530

    init

---
+++ src/day1/Day01.java
@@ -0,0 +1,12 @@
+package day1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day01 {
+
+	public static void main(String[] args) {
+		List<String> inputList = new ArrayList<>();
+
+	}
+}
