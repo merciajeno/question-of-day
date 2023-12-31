5a1a7bbfbe0d355659c98190f1e0d53b1f250385
I Mercia Jeno <113104600+merciajeno@users.noreply.github.com>
2023-12-31 12:05:57 +0530

    Day01.java

--- src/day1/Day01.java
+++ src/day1/Day01.java
@@ -33,6 +33,32 @@ public class Day01 {
 	//@formatter:on
 	private void process() {
 		// write your code here
+	        for(String input:inputList)//loops over the entire list of days
+		{
+		    int pointer = input.indexOf('=');//separator for the month and day
+		    String month = input.substring(0,pointer);
+		    String day = input.substring(pointer+1,input.length());
+		    
+		    if(monthWiseMaxDayMap.containsKey(month))//if contains then checks if it is maximum or not
+		    {
+		        String max_day = monthWiseMaxDayMap.get(month);
+		        int new_day = Integer.parseInt(day.substring(day.length()-2,day.length()));
+		        int input_day = Integer.parseInt(max_day.substring(max_day.length()-2,max_day.length()));
+		        if(new_day>input_day)
+	                {
+		          monthWiseMaxDayMap.put(month,day);
+	                }
+		    }
+		    else
+		    {
+		        monthWiseMaxDayMap.put(month,day);
+		    }
+		    
+		}
+		for(String key:monthWiseMaxDayMap.keySet())
+		{
+		    System.out.println("KEY:"+key+" "+"VALUE:"+monthWiseMaxDayMap.get(key));
+		}
 	}
 
 	private void initData() {
